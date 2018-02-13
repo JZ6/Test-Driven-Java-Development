577c4e39eb4c8b34576d1ec3c7bc1f84be637e52
JZ <yjzuo6@gmail.com>
2018-02-12 22:17:07 -0500

    Completed RetangularGrid

--- src/main/java/edu/toronto/csc301/grid/impl/RectangularGrid.java
+++ src/main/java/edu/toronto/csc301/grid/impl/RectangularGrid.java
@@ -3,25 +3,47 @@ package edu.toronto.csc301.grid.impl;
 import edu.toronto.csc301.grid.GridCell;
 import edu.toronto.csc301.grid.IGrid;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 
-public class RectangularGrid<T> implements IGrid {
+public class RectangularGrid<T> implements IGrid<T> {
 
-	private double x, y;
-	private int r;
+	private int w, h;
+	private GridCell sw;
+	private T t;
+	private ArrayList al;
 
-	public RectangularGrid(int x, int y, GridCell g) {
-
+	public RectangularGrid(int w, int h, GridCell sw) {
+		if (sw == null) {
+			throw new NullPointerException();
+		}
+		if (w <= 0 || h <= 0) {
+			throw new IllegalArgumentException();
+		}
+		this.w = w;
+		this.h = h;
+		this.sw = sw;
+		this.al = new ArrayList<GridCell>();
+		for (int i = this.sw.x; i < this.sw.x + this.w; i++) {
+			for (int j = this.sw.y; j < this.sw.y + this.h; j++) {
+				this.al.add(GridCell.at(i,  j));
+			}
+		}
 	}
 
 	public T getItem(GridCell cell){
-		return null;
+		if (cell.x > this.sw.x + this.w || cell.x < this.sw.x || cell.y > this.sw.y + this.h || cell.y < this.sw.y){
+			throw new IllegalArgumentException();
+		}
+		return t;
 	}
+
+
 	public Iterator<GridCell> getGridCells(){
-		return null;
+		return this.al.iterator();
 	}
 	public boolean hasCell(GridCell cell){
-		return true;
+		return this.al.contains(cell);
 	}
 }
