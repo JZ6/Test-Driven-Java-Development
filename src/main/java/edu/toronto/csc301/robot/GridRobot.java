e6a3fb6d8888f7c8c02e5e95447550e6f0a5f271
JZ <yjzuo6@gmail.com>
2018-03-04 18:46:20 -0500

    Completed step 1

--- src/main/java/edu/toronto/csc301/robot/GridRobot.java
+++ src/main/java/edu/toronto/csc301/robot/GridRobot.java
@@ -3,17 +3,32 @@ package edu.toronto.csc301.robot;
 import edu.toronto.csc301.grid.GridCell;
 
 public class GridRobot implements IGridRobot {
-	public GridRobot(GridCell G){
+	private GridCell location;
+
+	public GridRobot(GridCell g) {
+		if (g == null) {
+			throw new NullPointerException();
+		}
+		this.location = g;
 
 	}
 
 	public GridCell getLocation() {
-		return null;
+		return this.location;
 	}
 
 	public void step(Direction direction) {
-
+		if (direction == Direction.NORTH) {
+			this.location = GridCell.at(this.location.x, this.location.y + 1);
+		} else if (direction == Direction.EAST) {
+			this.location = GridCell.at(this.location.x + 1, this.location.y);
+		} else if (direction == Direction.SOUTH) {
+			this.location = GridCell.at(this.location.x, this.location.y - 1);
+		} else if (direction == Direction.WEST) {
+			this.location = GridCell.at(this.location.x - 1, this.location.y);
+		}
 	}
+
 
 	public void startListening(StepListener listener) {
 
--- src/main/java/edu/toronto/csc301/warehouse/Warehouse.java
+++ src/main/java/edu/toronto/csc301/warehouse/Warehouse.java
@@ -2,32 +2,57 @@ package edu.toronto.csc301.warehouse;
 
 import edu.toronto.csc301.grid.GridCell;
 import edu.toronto.csc301.grid.IGrid;
+import edu.toronto.csc301.robot.GridRobot;
 import edu.toronto.csc301.robot.IGridRobot;
+import edu.toronto.csc301.robot.IGridRobot.Direction;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.function.Consumer;
 
 public class Warehouse implements IWarehouse {
+	private IGrid<Rack> g;
+	private ArrayList<IGridRobot> r;
+	private Map<IGridRobot, Direction> rim;
 
-	public Warehouse(IGrid<Rack> grid){
-
+	public Warehouse(IGrid<Rack> grid) {
+		if (grid == null) {
+			throw new NullPointerException();
+		}
+		this.g = grid;
+		this.r = new ArrayList<IGridRobot>();
+		this.rim = new HashMap<IGridRobot, Direction>();
 	}
 
 	public IGrid<Rack> getFloorPlan() {
-		return null;
+		return this.g;
 	}
 
 	public IGridRobot addRobot(GridCell initialLocation) {
-		return null;
+
+		if (!this.g.hasCell(initialLocation)) {
+			throw new IllegalArgumentException();
+		}
+		this.r.forEach(robot -> {
+			if (robot.getLocation().equals(initialLocation)) {
+				throw new IllegalArgumentException();
+			}
+		});
+
+		IGridRobot gr = new GridRobot(initialLocation);
+		this.r.add(gr);
+
+		return gr;
 	}
 
 	public Iterator<IGridRobot> getRobots() {
-		return null;
+		return this.r.iterator();
 	}
 
-	public Map<IGridRobot, IGridRobot.Direction> getRobotsInMotion() {
-		return null;
+	public Map<IGridRobot, Direction> getRobotsInMotion() {
+		return rim;
 	}
 
 	public void subscribe(Consumer<IWarehouse> observer) {
