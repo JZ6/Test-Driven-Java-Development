82d5deaf7376f8e2c2c5537b83b2ff1b95ba0963
JZ <yjzuo6@gmail.com>
2018-03-04 20:48:35 -0500

    Completed step 2

--- src/main/java/edu/toronto/csc301/robot/GridRobot.java
+++ src/main/java/edu/toronto/csc301/robot/GridRobot.java
@@ -2,15 +2,18 @@ package edu.toronto.csc301.robot;
 
 import edu.toronto.csc301.grid.GridCell;
 
+import java.util.ArrayList;
+
 public class GridRobot implements IGridRobot {
 	private GridCell location;
+	private ArrayList<StepListener> listening;
 
 	public GridRobot(GridCell g) {
 		if (g == null) {
 			throw new NullPointerException();
 		}
 		this.location = g;
-
+		this.listening = new ArrayList<StepListener>();
 	}
 
 	public GridCell getLocation() {
@@ -18,6 +21,10 @@ public class GridRobot implements IGridRobot {
 	}
 
 	public void step(Direction direction) {
+		int i;
+		for (i = 0; i < listening.size(); i++) {
+			listening.get(i).onStepStart(this, direction);
+		}
 		if (direction == Direction.NORTH) {
 			this.location = GridCell.at(this.location.x, this.location.y + 1);
 		} else if (direction == Direction.EAST) {
@@ -27,14 +34,16 @@ public class GridRobot implements IGridRobot {
 		} else if (direction == Direction.WEST) {
 			this.location = GridCell.at(this.location.x - 1, this.location.y);
 		}
+		for (i = 0; i < listening.size(); i++) {
+			listening.get(i).onStepEnd(this, direction);
+		}
 	}
 
-
 	public void startListening(StepListener listener) {
-
+		listening.add(listener);
 	}
 
 	public void stopListening(StepListener listener) {
-
+		listening.remove(listener);
 	}
 }
--- src/main/java/edu/toronto/csc301/warehouse/Warehouse.java
+++ src/main/java/edu/toronto/csc301/warehouse/Warehouse.java
@@ -5,6 +5,7 @@ import edu.toronto.csc301.grid.IGrid;
 import edu.toronto.csc301.robot.GridRobot;
 import edu.toronto.csc301.robot.IGridRobot;
 import edu.toronto.csc301.robot.IGridRobot.Direction;
+import edu.toronto.csc301.robot.IGridRobot.StepListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,18 +13,22 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.function.Consumer;
 
-public class Warehouse implements IWarehouse {
+public class Warehouse implements IWarehouse, StepListener {
 	private IGrid<Rack> g;
 	private ArrayList<IGridRobot> r;
 	private Map<IGridRobot, Direction> rim;
+	private ArrayList<Consumer<IWarehouse>> observing;
 
-	public Warehouse(IGrid<Rack> grid) {
+	public Warehouse(IGrid<Rack> grid)
+
+	{
 		if (grid == null) {
 			throw new NullPointerException();
 		}
 		this.g = grid;
 		this.r = new ArrayList<IGridRobot>();
 		this.rim = new HashMap<IGridRobot, Direction>();
+		this.observing = new ArrayList<Consumer<IWarehouse>>();
 	}
 
 	public IGrid<Rack> getFloorPlan() {
@@ -41,8 +46,15 @@ public class Warehouse implements IWarehouse {
 			}
 		});
 
+		int i;
+		for (i = 0; i < this.observing.size(); i++) {
+			this.observing.get(i).accept(this);
+		}
+
 		IGridRobot gr = new GridRobot(initialLocation);
 		this.r.add(gr);
+
+		gr.startListening(this);
 
 		return gr;
 	}
@@ -52,14 +64,35 @@ public class Warehouse implements IWarehouse {
 	}
 
 	public Map<IGridRobot, Direction> getRobotsInMotion() {
-		return rim;
+		Map<IGridRobot, Direction> ret = new HashMap<IGridRobot, Direction>(rim);
+		return ret;
 	}
 
 	public void subscribe(Consumer<IWarehouse> observer) {
-
+		observing.add(observer);
 	}
 
 	public void unsubscribe(Consumer<IWarehouse> observer) {
+		observing.remove(observer);
+	}
+
+	@Override
+	public void onStepStart(IGridRobot robot, Direction direction) {
+		rim.put(robot, direction);
+		int i;
+		for (i = 0; i < this.observing.size(); i++) {
+			this.observing.get(i).accept(this);
+		}
+
+	}
+
+	@Override
+	public void onStepEnd(IGridRobot robot, Direction direction) {
+		rim.remove(robot, direction);
+		int i;
+		for (i = 0; i < this.observing.size(); i++) {
+			this.observing.get(i).accept(this);
+		}
 
 	}
 }
